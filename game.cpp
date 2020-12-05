d040b29956e9e7068b1b161f48261de16bae17be
vitorzera <73194198+vitorzera@users.noreply.github.com>
2020-12-05 14:37:16 -0300

    Add files via upload

--- game.cpp
+++ game.cpp
@@ -6,21 +6,20 @@ int main()
 {
 
 
-    const int windowWidth = 1100, windowHeight = 619;
+    const int windowWidth = 1100, windowHeight = 619; //Define a janela do jogo
 
-    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "trampo");
-    window.setFramerateLimit(60);
+    sf::RenderWindow window(sf::VideoMode(windowWidth, windowHeight), "Jogo_LEdeD");
+    window.setFramerateLimit(60); //Frmaes do jogo
 
 
-    sf::Texture bgTexture;
+    sf::Texture bgTexture; //Textura do background
     bgTexture.loadFromFile("imagens/background.jpg");
     bgTexture.setRepeated(true);
     sf::IntRect bgRect(0, 0, windowWidth, windowHeight);
     sf::Sprite bgSprite(bgTexture);
 
-    sf::Font font;
+    sf::Font font; //Textura do texto
     font.loadFromFile("imagens/octin.ttf");
-
     sf::Text text;
     text.setFont(font);
     text.setCharacterSize(40);
@@ -28,55 +27,55 @@ int main()
     text.setPosition(10, 10);
     text.setString("");
 
-    sf::Clock clock;
+    sf::Clock clock; //Timer
 
-    sf::Texture nvTexture;
+    sf::Texture nvTexture; //Textura da nave a esqueda
     nvTexture.loadFromFile("imagens/n1.png");
     sf::IntRect nvRect(4, 4, 65, 100);
     sf::Sprite nvSprite(nvTexture);
 
-    sf::Texture inv2Texture;
+    sf::Texture inv2Texture; //Textura da nave inimiga
     inv2Texture.loadFromFile("imagens/n2.png");
     sf::IntRect inv2Rect(177, 3, 78, 67);
     sf::Sprite inv2Sprite(inv2Texture);
 
-    sf::Texture inv3Texture;
+    sf::Texture inv3Texture; //Textura da nave inimiga
     inv3Texture.loadFromFile("imagens/n2.png");
     sf::IntRect inv3Rect(177, 3, 78, 67);
     sf::Sprite inv3Sprite(inv3Texture);
 
-    sf::Texture bnvTexture;
+    sf::Texture bnvTexture; //Textura da nave inimiga
     bnvTexture.loadFromFile("imagens/n3.png");
     sf::IntRect bnvRect(194, 0, 77, 99);
     sf::Sprite bnvSprite(bnvTexture);
 
-    sf::Texture bnv1Texture;
+    sf::Texture bnv1Texture; //Textura da nave inimiga
     bnv1Texture.loadFromFile("imagens/n3.png");
     sf::IntRect bnv1Rect(194, 0, 77, 99);
     sf::Sprite bnv1Sprite(bnv1Texture);
 
-    sf::Texture anvTexture;
+    sf::Texture anvTexture; //Textura da nave inimiga
     anvTexture.loadFromFile("imagens/n4.png");
     sf::IntRect anvRect(276, 2, 103, 93);
     sf::Sprite anvSprite(anvTexture);
 
-    sf::Texture shTexture;
+    sf::Texture shTexture; //textura dos tiros
     shTexture.loadFromFile("imagens/shot.png");
     sf::Sprite shSprite(shTexture);
 
-    sf::Texture sh1Texture;
+    sf::Texture sh1Texture; //textura dos tiros
     sh1Texture.loadFromFile("imagens/shot.png");
     sf::Sprite sh1Sprite(sh1Texture);
 
-    sf::Texture sh2Texture;
+    sf::Texture sh2Texture; //textura dos tiros
     sh2Texture.loadFromFile("imagens/shot.png");
     sf::Sprite sh2Sprite(sh2Texture);
 
-    sf::Texture sh3Texture;
+    sf::Texture sh3Texture; //textura dos tiros
     sh3Texture.loadFromFile("imagens/shot.png");
     sf::Sprite sh3Sprite(sh3Texture);
 
-    sf::Texture sh4Texture;
+    sf::Texture sh4Texture; //textura dos tiros
     sh4Texture.loadFromFile("imagens/shot.png");
     sf::Sprite sh4Sprite(sh4Texture);
 
@@ -84,7 +83,7 @@ int main()
 
     float invY = windowHeight-350, nvY = windowHeight-350, shX = windowWidth;
     bool gameOver = false;
-    int steps=5;
+    int steps=0;
 
     // game loop
     while (window.isOpen())
@@ -97,7 +96,7 @@ int main()
                 window.close();
             else if (event.type == sf::Event::KeyPressed)
              {
-                if (event.key.code == sf::Keyboard::Escape)
+                if (event.key.code == sf::Keyboard::Escape) //Fecha o jogo
                     window.close();
              }
 
@@ -105,7 +104,7 @@ int main()
         }
 
         // Atualização do estado do jogo
-        if(!gameOver)
+        if(!gameOver) //Tela de GameOver
         {
 
         bgRect.left++;
@@ -117,7 +116,7 @@ int main()
           text.setString(std::to_string(steps));
           clock.restart();
         }
-        
+
           nvSprite.setTextureRect(nvRect); // nave do player
           nvSprite.setPosition(25, nvY);
 
